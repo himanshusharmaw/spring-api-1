d9a1e30aacdd4c2a580f4099ac23e6f0cc9e1b0c
holaamigoaadhar <testmeaadhaarservices@gmail.com>
2025-04-28 07:45:58 +0530

    updated jar

--- src/main/java/com/aadhaarservices/aadhaar_services/config/CorsConfig.java
+++ src/main/java/com/aadhaarservices/aadhaar_services/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
     @Bean(name = "customCorsConfigurationSource")
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // React frontend
+        config.setAllowedOrigins(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);

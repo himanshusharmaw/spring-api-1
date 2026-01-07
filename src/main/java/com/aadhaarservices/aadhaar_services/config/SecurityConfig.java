4fc51bf178bfc42a52b8e0df5719b5853f9ae386
holaamigoaadhar <testmeaadhaarservices@gmail.com>
2026-01-07 11:37:18 +0530

    initial commit

--- src/main/java/com/aadhaarservices/aadhaar_services/config/SecurityConfig.java
+++ src/main/java/com/aadhaarservices/aadhaar_services/config/SecurityConfig.java
@@ -55,13 +55,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
-            		.requestMatchers(
-            			    "/api/auth/login",
-            			    "/api/auth/register",
-            			    "/api/admin/login"
-            			).permitAll()
-
-                .requestMatchers("/api/user/upload-photo").authenticated()
+                .requestMatchers("/login", "/api/admin/login").permitAll()
                 .requestMatchers("/api/user/profile").authenticated()
                 .anyRequest().permitAll()
             )
